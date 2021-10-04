2720630fae73f348230eadf913258a2b64deb4ec
Ahmet <ahmedovv123@gmail.com>
2021-10-04 17:10:25 +0300

    update: server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // simple route for
 app.get("/", (req,res) => {
-    res.json({message: "Welcome to server"});
+    res.json({message: "Welcome"});
 });
 
 require("./app/routes/node.routes")(app);
